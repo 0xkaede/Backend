447864a743701a5ea911649e4d3e49612e0190a1
kaede <masseyj218@gmail.com>
2023-09-10 16:30:40 +0100

    Added TimeLine, Fix Folder Location

---
+++ KaedeBackend/Models/Calendar/Timeline.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using KaedeBackend.Models.Calendar;
+
+namespace FortniteDotNet.Models.FortniteService.Calendar
+{
+    public class Timeline
+    {
+        [JsonProperty("channels")]
+        public Dictionary<string, TimelineChannel> Channels { get; set; }
+
+        [JsonProperty("currentTime")]
+        public string CurrentTime { get; set; }
+
+        [JsonProperty("cacheIntervalMins")]
+        public double CacheIntervalMinutes { get; set; }
+    }
+}
---
+++ KaedeBackend/Models/Calendar/TimelineChannel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace KaedeBackend.Models.Calendar
+{
+    public class TimelineChannel
+    {
+        [JsonProperty("states")]
+        public List<ChannelState> States { get; set; }
+
+        [JsonProperty("cacheExpire")]
+        public string CacheExpire { get; set; }
+    }
+}
